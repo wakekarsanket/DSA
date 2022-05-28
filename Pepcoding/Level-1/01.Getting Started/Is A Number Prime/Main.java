e73757b056b9e7aab8af439ff672b24a9ef112f2
wakekarsanket <sanketwakekar@outlook.com>
2022-05-28 18:53:32 +0530

    Getting Started

---
+++ Pepcoding/Level-1/01.Getting Started/Is A Number Prime/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+  
+       // write ur code here
+       int t = scn.nextInt();
+
+       for(int i=1;i<=t;i++){
+           int n = scn.nextInt();
+           int factor = 0;
+           for(int div=2;div*div<=n;div++){               //div<=(int)Math.sqrt(n);
+               if(n % div == 0){               
+               factor++;
+               break;
+               }
+           }
+           if(factor>0){
+               System.out.println("not prime");
+           }else{
+               System.out.println("prime");
+           }
+       }
+  
+   }
+  }
