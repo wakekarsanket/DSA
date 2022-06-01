7c5f3735c2d472920d4b19248d221b0c26e59de6
wakekarsanket <sanketwakekar@outlook.com>
2022-06-01 13:40:21 +0530

    added

---
+++ Pepcoding/Level-1/02.2D Array/The State Of Wakanda - 2/Main.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        for (int gap = 0; gap < arr[0].length; gap++) {
+            for (int i = 0, j = gap; i < arr.length && j < arr[0].length; i++, j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+
+    }
+
+}
