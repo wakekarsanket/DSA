feb5b6dbf2f210462609a49805e7c9e91a5fda1a
wakekarsanket <sanketwakekar@outlook.com>
2022-06-01 13:06:13 +0530

    added

---
+++ Pepcoding/Level-1/02.2D Array/Rotate By 90 Degree/Main.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int arr[][] = new int[n][n];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        // transpose
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        // reversing of every row
+        for (int r = 0; r < arr.length; r++) {
+            int i = 0, j = arr[0].length - 1;
+
+            while (i < j) {
+                int temp = arr[r][i];
+                arr[r][i] = arr[r][j];
+                arr[r][j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        display(arr);
+    }
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
