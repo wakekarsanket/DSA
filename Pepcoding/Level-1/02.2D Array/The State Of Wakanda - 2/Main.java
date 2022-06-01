07966c1a0666563c8c79cf173bad21147d99fdce
wakekarsanket <sanketwakekar@outlook.com>
2022-06-01 18:38:09 +0530

    added

--- Pepcoding/Level-1/02.2D Array/The State Of Wakanda - 2/Main.java
+++ Pepcoding/Level-1/02.2D Array/The State Of Wakanda - 2/Main.java
@@ -15,6 +15,14 @@ public class Main {
             }
         }
 
+        //lower matrix
+        // for (int gap = arr.length-1; gap > 0; gap--) {
+        //     for (int i = gap, j = 0; i < arr.length && j < arr[0].length; i++, j++) {
+        //         System.out.println(arr[i][j]);
+        //     }
+        // }
+
+        //upper matrix
         for (int gap = 0; gap < arr[0].length; gap++) {
             for (int i = 0, j = gap; i < arr.length && j < arr[0].length; i++, j++) {
                 System.out.println(arr[i][j]);
