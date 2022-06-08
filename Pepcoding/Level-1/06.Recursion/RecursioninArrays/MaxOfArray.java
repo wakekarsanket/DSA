de2fd699a33c63f28673ed3058662a76ba940ff7
wakekarsanket <sanketwakekar@outlook.com>
2022-06-08 13:02:23 +0530

    added

--- Pepcoding/Level-1/06.Recursion/RecursioninArrays/MaxOfArray.java
+++ Pepcoding/Level-1/06.Recursion/RecursioninArrays/MaxOfArray.java
@@ -11,6 +11,21 @@ public class MaxOfArray {
         return (me > recAns) ? me : recAns;
     }
 
+    public static int minOfArray(int[] arr, int idx){
+        // if(idx==arr.length-1){
+        //     return arr[idx];
+        // }
+        //OR
+        if(idx==arr.length){
+            return Integer.MAX_VALUE;
+        }
+        int min = minOfArray(arr,idx+1);
+        if(arr[idx]<min){
+            min = arr[idx];
+        }
+        return min;
+    }
+
     public static void main(String args[]) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
