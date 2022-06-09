661991f3a4ad6c893aa79424923f3e321981efb5
wakekarsanket <sanketwakekar@outlook.com>
2022-06-09 17:22:33 +0530

    added

---
+++ Pepcoding/Level-1/03.Function and Arrays/Span Of Array/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+        int span = max - min;
+        System.out.println(span);
+    }
+
+}
